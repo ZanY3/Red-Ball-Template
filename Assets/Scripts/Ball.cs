068fdb2d0bc7d94a1c42d86b6e93fea16af6d9ad
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2023-11-19 18:23:29 +0200

    HP

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Ball : MonoBehaviour
 {
     public float jump;
     public float moveForce;
     Rigidbody2D rb;
+    bool isGrounded;
+
+    public float partCount;
+    public GameObject particle;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();    
@@ -19,10 +24,36 @@ public class Ball : MonoBehaviour
         //rb.velocity = new Vector3(hor, 0);
         rb.AddForce(new Vector3(hor, 0) * moveForce );
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity += Vector2.up * jump;
         }
         
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            for (int i = 0; i < partCount; i++)
+            {
+                var offset = Random.insideUnitSphere;
+                Instantiate(particle, transform.position + offset, transform.rotation);
+            }
+            FindObjectOfType<GameManager>().Lose();
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Contains("Teleporter"))
+        {
+            FindObjectOfType<GameManager>().Win();
+        }
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class GameManager : MonoBehaviour
+{
+    public int hp;
+    public int coins;
+    public int currentLevel;
+    public bool hasWon;
+    public List<string> levels;
+
+    float targetTrasitionScale;
+    public Transform Transition;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        var targetV3 = Vector3.one * targetTrasitionScale;
+        Transition.localScale = Vector3.MoveTowards(Transition.localScale,targetV3,60 * Time.deltaTime);
+    }
+    public void Win()
+    {
+        if (hasWon) return;
+
+        hasWon = true;
+        currentLevel++;
+        targetTrasitionScale = 30;
+        Invoke("LoadNextScene",1f);
+    }
+    public void Lose()
+    {
+        hp--;
+
+        if (hp <= 3)
+        {
+            SceneManager.LoadScene(currentLevel);
+        }
+        if (hp <= 0)
+        {
+            SceneManager.LoadScene(levels[0]);
+            hp = 3;
+        }
+    }
+    public void Coins()
+    {
+        coins++;
+    }
+    public void LoadNextScene()
+    {
+        var Lname = levels[currentLevel];
+        SceneManager.LoadScene(Lname);
+        hasWon = false;
+        targetTrasitionScale = 0;
+
+    }
+}
